e241929a3185ec24cdef26a8b971759e991df2f5
mantzaris <avmantzaris@gmail.com>
2016-12-20 18:48:41 +0000

    added the figure of a grayscale image of the linear network over the time interval of the polarization

--- cobbsDiscrete1.py
+++ cobbsDiscrete1.py
@@ -2,13 +2,14 @@ import numpy
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import pylab as P
+from pylab import *
 #extending the Loren Cobbs models to the discrete case for political modelling
 
 ####NETWORK SETUP
 #time points
-time_pnts = 14
+time_pnts = 5
 #number of nodes in linear network
-u_N = 15
+u_N = 5
 #nodes in the linear network
 network = numpy.random.rand(1,u_N)
 network_tmp = numpy.zeros((1,u_N))
@@ -39,7 +40,8 @@ for tt in range(1,time_pnts):
         elif (ii == (u_N-1)):
             neighbors_i = network_total[tt-1,u_N-2]
         else:
-            neighbors_i = 0.5*((network_total[tt-1,ii-1]) + (network_total[tt-1,ii+1]))
+            neighbors_i = 0.5*((network_total[tt-1,ii-1]) \
+                               + (network_total[tt-1,ii+1]))
         #the feedback effect to return to natural internal state G upon deviation
         fb_i = RR * (GG - u_i)                  
         #contribution of neighbors upon polarisation extent
@@ -55,14 +57,18 @@ for tt in range(1,time_pnts):
     network_total[tt,:] = network_tmp[0,:]    
     #plot the histogram of the u_i after each iteration
 binspaces = [ii/10 for ii in range(0,11)]
-#P.figure()
-print(network_total)
 f,axarr = plt.subplots(time_pnts,sharex=True)
+network_gryscle = numpy.zeros((time_pnts,len(binspaces)-1))
 for ii in range(time_pnts):
-    axarr[ii].hist(network_total[ii,:], binspaces,normed=1,histtype='bar')
+    nn,bb,pp = axarr[ii].hist(network_total[ii,:], binspaces,normed=1,histtype='bar')
+    network_gryscle[ii,:] = (nn / numpy.sum(nn))
     axarr[ii].set_title('iteration:'+str(ii))
     #axarr[ii].xlabel('u')
 f.suptitle('RR value='+str(RR)+', EE value='+str(EE)+', DD value='+str(DD))
+#P.show()
+#P.figure()
+#grayscale/heatmap of the number of nodes occupying each bin of the spectrum
+figure()
+plt.imshow(network_gryscle,cmap='gray')
+f.suptitle('RR value='+str(RR)+', EE value='+str(EE)+', DD value='+str(DD))
 P.show()
-#axarr[0].hist(network_total[0,:], binspaces,normed=1,histtype='bar')
-#axarr[1].hist(network_total[1,:], binspaces,normed=1,histtype='bar')
